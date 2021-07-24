4b8137ca5a85460d3e37dec6e4ea7dc8098adb28
Alex Waller <alex@artofalexwaller.com>
2021-07-24 15:45:10 -0400

    settingUpNewDachshundRoboHybridClass

---
+++ src/com/company/DachshundsAndHybridsAndRobots.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class DachshundsAndHybridsAndRobots {
+
+    public static void main(String[] args){
+
+        String ramHybrid1 = "Tony";
+        String ramHybrid2 = "Otto";
+        String ramHybrid3 = "Jay";
+        String ramHybrid4 = "Stacey";
+
+        String treeHybrid1 = "Cornelius";
+        String treeHybrid2 = "Magnus";
+        String treeHybrid3 = "Owen";
+        String treeHybrid4 = "Everett";
+        String treeHybrid5 = "Jasper";
+
+        String troutHybrid1 = "Rainbow";
+        String troutHybrid2 = "Brown";
+
+        String robot1 = "Hank-44";
+        String robot2 = "Warren-21";
+        String robot3 = "Mellon-Tech";
+        String robot4 = "Eggplant-Head";
+
+        double ramHybridNo = 4;
+        double treeHybridNo = 5;
+        double troutHybridNo = 2;
+        double robotNo = 4;
+
+        String[] rams = {"Tony", "Otto", "Jay", "Stacey"};
+        String[] trees = {"Cornelius", "Magnus", "Owen", "Everett", "Jasper"};
+        String[] trout = {"Rainbow", "Brown"};
+        String[] robots = {"Hank-44", "Warren-21", "Mellon-Tech", "Eggplant-Head"};
+        String[] dachshunds = {"Beezer", "Chipper", "Bodhi", "Jake", "Star"};
+
+
+    }
+}
