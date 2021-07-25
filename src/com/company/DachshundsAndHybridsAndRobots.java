e9cbc87ffd8be0f6ad076f2c1c67609c0b46571a
Alex Waller <alex@artofalexwaller.com>
2021-07-24 22:13:06 -0400

    roboTroutReverseStrings

--- src/com/company/DachshundsAndHybridsAndRobots.java
+++ src/com/company/DachshundsAndHybridsAndRobots.java
@@ -132,17 +132,17 @@ public class DachshundsAndHybridsAndRobots {
 
         System.out.println(trout[0]);
 
-        if(dachshunds.length == 5 && trees.length == 5){
-            for(int d1 = 0; d1 < dachshunds.length; d1++){
-                if(d1 == 2){
+        if (dachshunds.length == 5 && trees.length == 5) {
+            for (int d1 = 0; d1 < dachshunds.length; d1++) {
+                if (d1 == 2) {
                     System.out.println(dachshunds[d1] + " wants to do something!!!!");
                 }
             }
         }
 
-        if(robots[2] == "Mellon-Tech" || trout.length * trees.length == 37){
-            for(int r1 = 0; r1 < robots.length; r1++){
-                if(r1 == 3){
+        if (robots[2] == "Mellon-Tech" || trout.length * trees.length == 37) {
+            for (int r1 = 0; r1 < robots.length; r1++) {
+                if (r1 == 3) {
                     System.out.println(robots[r1] + " is not an Eggplant!?");
                 }
             }
@@ -154,18 +154,17 @@ public class DachshundsAndHybridsAndRobots {
         System.out.println(robotNo);
         System.out.println(dachshundNo);
 
-        if(treeHybridNo > dachshundNo){
+        if (treeHybridNo > dachshundNo) {
             System.out.println(trees[0]);
-        }
-        else
+        } else
             System.out.println("There are the same amount of trees and dachshunds!");
 
         System.out.println(dachshunds.length);
         System.out.println(dachshunds[0].chars().count());
         System.out.println(dachshunds[1].chars().count());
 
-        if (dachshunds[0].chars().count() == 6){
-            for(int d1 = 0; d1 < dachshunds.length; d1++){
+        if (dachshunds[0].chars().count() == 6) {
+            for (int d1 = 0; d1 < dachshunds.length; d1++) {
                 System.out.println(dachshunds[d1].chars().count());
             }
         }
@@ -202,25 +201,86 @@ public class DachshundsAndHybridsAndRobots {
         int ii = 37;
         System.out.println(trees[0].chars().count());
 
-        if(trees.length == 5){
-            for (int t1 = 0; t1 < trees.length; t1++){
+        if (trees.length == 5) {
+            for (int t1 = 0; t1 < trees.length; t1++) {
                 System.out.println(trees[t1].chars().count());
             }
         }
 
-        StringBuilder reverseTron = new StringBuilder();
-        // definitely need a string builder in order to reverse a string
 
-        reverseTron.append(dachshunds[0]);
-        // we the .append() method to attach the string we want to reverse
+        if (dachshunds[0] == "Beezer") {
 
-        reverseTron = reverseTron.reverse();
-        // new reverseTron equals the old reverseTron reversed
+            StringBuilder reverseTron = new StringBuilder();
+            // definitely need a string builder in order to reverse a string
+
+            reverseTron.append(dachshunds[0]);
+            // we the .append() method to attach the string we want to reverse
+
+            reverseTron = reverseTron.reverse();
+            // new reverseTron equals the old reverseTron reversed
 
 
-        System.out.println(reverseTron);
-        // simply print out the string here
+            System.out.println(reverseTron);
+            // simply print out the string here
+        }
 
+        if (trees.length == 5) {
+            for (int t1 = 0; t1 < trees.length; t1++) {
+                StringBuilder reverseTron = new StringBuilder();
+                // definitely need a string builder in order to reverse a string
+                reverseTron.append(trees[t1]);
+                reverseTron = reverseTron.reverse();
+                System.out.println(reverseTron);
+            }
+        }
+
+        if (rams.length == 4) {
+            for (int r1 = 0; r1 < rams.length; r1++) {
+                StringBuilder reverseTron = new StringBuilder();
+                // making a new instance of the StringBuilder class
+                // definitely need a string builder in order to reverse a string
+                reverseTron.append(rams[r1]);
+                reverseTron = reverseTron.reverse();
+                System.out.println(reverseTron);
+
+            }
+        }
+
+        if (robots.length == 4) {
+            for (int r1 = 0; r1 < robots.length; r1++) {
+                StringBuilder roboReverse = new StringBuilder();
+                // making a new instance of the StringBuilder class
+                // definitely need a string builder in order to reverse a string
+                roboReverse.append(robots[r1]);
+                roboReverse = roboReverse.reverse();
+                System.out.println(roboReverse);
+            }
+        }
+
+        if (trout.length == 2) {
+            for (int t1 = 0; t1 < trout.length; t1++) {
+                StringBuilder reverseTrout = new StringBuilder();
+                // making a new instance of the StringBuilderClass
+                // definitely need a string builder in order to reverse a string
+                reverseTrout.append(trout[t1]);
+                reverseTrout = reverseTrout.reverse();
+                System.out.println(reverseTrout);
+            }
+
+
+        }
+
+        if(robots.length == 4 && robots[2] == "Mellon-Tech"){
+            for(int r1 = 0; r1 < robots.length; r1++){
+                if(r1 == 2){
+                    StringBuilder robo = new StringBuilder();
+                    // new instance of string builder class
+                    robo.append(robots[r1]);
+                    robo = robo.reverse();
+                    System.out.println(robo);
+                }
+            }
+        }
 
     }
 }
