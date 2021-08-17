3bc6d6837d6f54a96e25158a1752b1b4bd5c482b
Alex Waller <alex@artofalexwaller.com>
2021-08-16 20:47:17 -0400

    DietPickerelCola//

--- src/com/company/DietPickerelCola.java
+++ src/com/company/DietPickerelCola.java
@@ -3,9 +3,10 @@ package com.company;
 public class DietPickerelCola {
     public String pickerelColaCount(String stCroix, String bigLake){
         String pickerel4 = "";
+        String pickerel5 = "St. Croix River";
         int pickerel1 = stCroix.length();
         int pickerel2 = bigLake.length();
-        int pickerel3 = pickerel1 + pickerel1;
+        int pickerel3 = pickerel1 + pickerel2;
         if(pickerel3 % Math.sqrt(pickerel3) == 0){
             pickerel4 = pickerel3 + " has a whole number for a square root.";
 
@@ -14,5 +15,6 @@ public class DietPickerelCola {
             pickerel4 = pickerel3 + " does not have a whole number for a square root.";
 
         return pickerel4;
+
     }
 }
