e4e5a556c865fcea0d2908b8dc41ba67f7b22747
Alex Waller <alex@artofalexwaller.com>
2021-08-25 11:45:18 -0400

    LakeCushetunkHeron//

--- src/com/company/CushetunkHeronArray.java
+++ src/com/company/CushetunkHeronArray.java
@@ -7,8 +7,9 @@ public class CushetunkHeronArray {
         for(int hh1 = 0; hh1 < riverHeron.length; hh1++){
             for(int hh2 = hh1 + 1; hh2 < riverHeron.length; hh2++){
                 if(target - riverHeron[hh1]  == riverHeron[hh2]){
-                    newHerons = new int[] {hh1, hh2};
+                    newHerons =  new int[] {hh1, hh2};
                     return newHerons;
+
                 }
             }
         }
