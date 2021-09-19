c0f0f98be47b2cad8e6d95bac356ed00410c0f70
Alex Waller <alex@artofalexwaller.com>
2021-09-18 22:32:46 -0400

    TroubleShooting

--- src/com/company/PickerelColaPremiumCustomer.java
+++ src/com/company/PickerelColaPremiumCustomer.java
@@ -2,22 +2,22 @@ package com.company;
 
 public class PickerelColaPremiumCustomer {
     String pickerelCustomerString(String pickerelCustomerType, double pickerelPrice){
-        pickerelCustomerType = pickerelCustomerType.toLowerCase();
+        String pickerelCustomerType1 = pickerelCustomerType.toLowerCase();
         String returnString = "";
         // so it can read entry even there are uppercase characters
         // Customer Types are Surface, Seaweed, and Snag
         // Surface gets 10% off
-        if (pickerelCustomerType == "surface"){
+        if (pickerelCustomerType1 == "surface"){
             pickerelPrice = pickerelPrice - (pickerelPrice * .10);
             returnString = "The price is " + pickerelPrice;
         }
         // Seaweed gets 15% off
-        else if (pickerelCustomerType == "seaweed"){
+        else if (pickerelCustomerType1 == "seaweed"){
             pickerelPrice = pickerelPrice - (pickerelPrice * .15);
             returnString = "The price is " + pickerelPrice;
         }
         // Snag gets 20% off
-        else if (pickerelCustomerType == "snag"){
+        else if (pickerelCustomerType1 == "snag"){
             pickerelPrice = pickerelPrice - (pickerelPrice * .20);
             returnString = "The price is " + pickerelPrice;
         }
--- src/com/company/rimRockAlgorithm.java
+++ src/com/company/rimRockAlgorithm.java
@@ -4,9 +4,10 @@ import java.util.Locale;
 
 public class rimRockAlgorithm {
     public String rimRock2(String rimRock1){
-        rimRock1 = rimRock1.toLowerCase();
+       String rimRock3 = rimRock1.toLowerCase();
 
-        if (rimRock1 == "pickerel") return "Working";
+
+        if (rimRock3 == "pickerel") return "Working";
 
     return "Not Working";
     }
