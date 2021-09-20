b46701ff9ae521eb84d3f447930b5d6ac92ef874
Alex Waller <alex@artofalexwaller.com>
2021-09-20 12:56:57 -0400

    ArrayPractice//

--- src/com/company/ArrayPractice.java
+++ src/com/company/ArrayPractice.java
@@ -6,7 +6,19 @@ public class ArrayPractice {
     public static void main(String[] args){
         int[] numbers = new int[3];
         // 3 is the length of the array
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
         System.out.println(Arrays.toString(numbers));
         // [0, 0, 0]
+        // all elements in numerical array are initialized to zero
+        // [10, 20, 30]
+        int[] numbers2 = {10, 20, 30};
+        System.out.println(Arrays.toString(numbers2));
+        // [10, 20, 30]
+        System.out.println(numbers.length);
+        // 3
+        System.out.println(numbers2.length);
+        // 3
     }
 }
