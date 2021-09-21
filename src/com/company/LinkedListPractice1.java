cb95a7d7a4303e220b3c0ded3ebc2a0816245d42
Alex Waller <alex@artofalexwaller.com>
2021-09-21 17:32:23 -0400

    LinkedListPractice1//

--- src/com/company/LinkedListPractice1.java
+++ src/com/company/LinkedListPractice1.java
@@ -7,8 +7,13 @@ public class LinkedListPractice1 {
         LinkedList list = new LinkedList();
         list.add(10);
         list.add(20);
-        list.add(30);
+        list.addLast(30);
+
         System.out.println(list);
         // [10, 20, 30]
+        // this is LinkedList, not an array despite the square brackets
+        list.addFirst(37);
+        System.out.println(list);
+        // [37, 10, 20, 30]
     }
 }
